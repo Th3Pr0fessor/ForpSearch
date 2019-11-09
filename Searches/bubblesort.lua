a836c01e43ffbe5ad965f708789f1d7054bd38e4
Th3Pr0fessor <47043384+Th3Pr0fessor@users.noreply.github.com>
2019-11-08 21:15:51 -0500

    Update bubblesort.lua

--- Searches/bubblesort.lua
+++ Searches/bubblesort.lua
@@ -1,1 +1,17 @@
-print("Bubble")
+local Ran = math.random
+local Table = {Ran(1, 100), Ran(1, 100), Ran(1, 100), Ran(1, 100), Ran(1, 100), Ran(1, 100), Ran(1, 100), Ran(1, 100), Ran(1, 100), Ran(1, 100)}
+for x = 1, math.ceil(#Table/2)+1 do
+	print("Trial: "..x)
+	for i = #Table, 1, -1 do
+		local Value_1, Value_2 = Table[i], Table[i-1]
+		print(Value_1)
+		if i ~= 1 then
+			if Value_2 > Value_1  then
+				print(Value_2, Value_1, "Switched!")
+				Table[i-1], Table[i] = Value_1, Value_2
+			end
+		end
+		wait(.1)
+	end
+	print(" ")
+end
